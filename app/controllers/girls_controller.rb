5f27eda86f3f9bdf32fbc4803fca5b8aa4631ba2
benoitgraziani <benoit.c.graziani@hotmail.fr>
2021-10-22 09:46:30 +0200

    e

--- app/controllers/girls_controller.rb
+++ app/controllers/girls_controller.rb
@@ -2,4 +2,20 @@ class GirlsController < ApplicationController
     def index
         @girls = Girl.all
     end
+
+    def new
+        @girl = Girl.new
+    end
+
+    def create
+        @girl = Girl.new(girl_params)
+        @girl.save
+        redirect_to root_path
+    end
+
+    private
+
+    def girl_params
+        params.require(:girl).permit(:name, :address, :content)
+    end
 end
